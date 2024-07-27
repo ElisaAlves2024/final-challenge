a33a025f3c0ed8479471acc30f136d2fce41654d
ElisaAlves2024 <alveselisarodrigues@gmail.com>
2024-07-27 13:41:51 +0200

    first commit

---
+++ src/test/java/bootcamp/dio/java/final_challenge/FinalChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package bootcamp.dio.java.final_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
